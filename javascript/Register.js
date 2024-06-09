5696f2b5ce1742c6d6b065aeda4af17f63a6c3bf
EugeneMaz10 <eugenengaira863@gmail.com>
2024-06-09 20:53:12 +0300

    update

---
+++ javascript/Register.js
@@ -0,0 +1,14 @@
+
+  function redirect()
+  {
+    // var url = "https://playstationdapp.on.fleek.co/Playstation";
+    //window.location(url);
+
+    var url2 = " https://www.playstation.com/nl-nl/";
+    window.location(url2);
+
+
+
+  }
+
+
