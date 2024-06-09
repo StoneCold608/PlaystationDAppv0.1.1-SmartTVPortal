dddfdfebeaa1acb08b22d37318dfa8dd4e6700d7
EugeneMaz10 <eugenengaira863@gmail.com>
2024-06-09 21:21:09 +0300

    policy check

--- javascript/Register.js
+++ javascript/Register.js
@@ -9,6 +9,15 @@
 
 
 
+
   }
+
+  function addTrialToken(tokenContents) {
+    const tokenElement = document.createElement('meta');
+    tokenElement.httpEquiv = 'origin-trial';
+    tokenElement.content = tokenContents;
+    document.head.appendChild(tokenElement);
+  }
+
 
 
