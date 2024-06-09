35cd2e767e66a69ccb7102d500b0fa7e6a6fac00
EugeneMaz10 <eugenengaira863@gmail.com>
2024-06-09 20:56:02 +0300

    policy check

--- javascript/Register.js
+++ javascript/Register.js
@@ -1,11 +1,11 @@
 
   function redirect()
   {
-    // var url = "https://playstationdapp.on.fleek.co/Playstation";
-    //window.location(url);
+     var url = "https://playstationdapp.on.fleek.co/Playstation";
+    window.location(url);
 
-    var url2 = " https://www.playstation.com/nl-nl/";
-    window.location(url2);
+    //var url2 = " https://www.playstation.com/nl-nl/";
+    //window.location(url2);
 
 
 
