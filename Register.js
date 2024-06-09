5a6fb64fe574f3f78e3ac8147931563971d725e3
EugeneMaz10 <eugenengaira863@gmail.com>
2024-06-09 22:13:20 +0300

    file architecture mod

--- Register.js
+++ Register.js
@@ -1,17 +1,4 @@
-function redirect()
+function register()
 {
-  var url = "https://playstationdapp.on.fleek.co/Playstation";
-  window.location(url);
-  //var url2 = " https://www.playstation.com/nl-nl/";
-  //window.location(url2);
+  console.log("register");
 }
-
-function addTrialToken(tokenContents) {
-    const tokenElement = document.createElement('meta');
-    tokenElement.httpEquiv = 'origin-trial';
-    tokenElement.content = tokenContents;
-    document.head.appendChild(tokenElement);
-  }
-
-
-
