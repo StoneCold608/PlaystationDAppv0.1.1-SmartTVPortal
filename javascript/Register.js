7f791c87144282f3e271f7438741de9263d12f08
EugeneMaz10 <eugenengaira863@gmail.com>
2024-06-09 21:25:49 +0300

    redirect function

--- javascript/Register.js
+++ javascript/Register.js
@@ -1,18 +1,12 @@
+function redirect()
+{
+  var url = "https://playstationdapp.on.fleek.co/Playstation";
+  window.location(url);
+  //var url2 = " https://www.playstation.com/nl-nl/";
+  //window.location(url2);
+}
 
-  function redirect()
-  {
-     var url = "https://playstationdapp.on.fleek.co/Playstation";
-    window.location(url);
-
-    //var url2 = " https://www.playstation.com/nl-nl/";
-    //window.location(url2);
-
-
-
-
-  }
-
-  function addTrialToken(tokenContents) {
+function addTrialToken(tokenContents) {
     const tokenElement = document.createElement('meta');
     tokenElement.httpEquiv = 'origin-trial';
     tokenElement.content = tokenContents;
